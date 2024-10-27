142920917479dba51b554e6419dc4eec2c51c67b
Loner9 <38490560+loner9@users.noreply.github.com>
2024-10-28 02:09:30 +0700

    InventoryItem.cs
    script for inventory item SO

---
+++ Assets/OurLastHope/Scripts/InventoryItem.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Weapon,
+    Kit,
+    KeyItem,
+    Note
+}
+
+[CreateAssetMenu(menuName = "ScriptableObjects/InventoryItem")]
+
+public class InventoryItem : ScriptableObject
+{
+    [Header("Config")]
+    public string ID;
+    public Sprite Icon;
+    public string Name;
+    [TextArea] public string Description;
+
+    [Header("Info")]
+    public ItemType ItemType;
+    public bool IsConsumable;
+    public bool IsStackable;
+    public int MaxStack;
+
+    [HideInInspector] public int MaxQuantity;
+
+    public InventoryItem CopyItem()
+    {
+        InventoryItem instance = Instantiate(this);
+        return instance;
+    }
+
+    public virtual bool UseItem()
+    {
+        return true;
+    }
+
+    public virtual bool EquipItem()
+    {
+        return true;
+    }
+
+    public virtual bool RemoveItem()
+    {
+        return true;
+    }
+
+}
