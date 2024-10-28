735d1a8897f2b1d5d21df744dcec38bfea3c5225
denx4x <dendymaulanaibrahim@gmail.com>
2024-10-28 10:26:42 +0700

    Push Project to github

---
+++ Assets/OurLastHope/Scripts/Dendy/Attack Melee & Range/Health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Logika untuk menghancurkan objek atau mengatur ulang statusnya
+        Destroy(gameObject);
+    }
+}
