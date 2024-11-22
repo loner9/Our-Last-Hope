8a6decd83709879dd870c14d890f7ea548e6076a
denx4x <dendymaulanaibrahim@gmail.com>
2024-11-22 19:04:25 +0700

    Build new function level selection in dialogue script clone

---
+++ Assets/OurLastHope/Scripts/Dendy/Level Selection/ResponseLevelHandler.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class ResponseLevelHandler : MonoBehaviour
+{
+    [SerializeField] private RectTransform responseBox;
+    [SerializeField] private GameObject responseContainerPrefab;
+    [SerializeField] private GameObject responseButtonTemplatePrefab;
+
+    private DialogueLevelUI dialogueUI;
+    private ResponseEvent[] responseEvents;
+
+    private List<GameObject> tempResponseContainers = new List<GameObject>();
+
+    private void Start()
+    {
+        dialogueUI = GetComponent<DialogueLevelUI>();
+    }
+
+    public void AddResponseEvents(ResponseEvent[] responseEvents)
+    {
+        this.responseEvents = responseEvents;
+    }
+
+    public void ShowResponses(Response[] responses)
+    {
+        foreach (Response response in responses)
+        {
+            // Instantiate responseContainer
+            GameObject responseContainer = Instantiate(responseContainerPrefab, responseBox.transform);
+            responseContainer.SetActive(true);
+
+            // Instantiate responseButtonTemplate inside the responseContainer
+            GameObject responseButton = Instantiate(responseButtonTemplatePrefab, responseContainer.transform);
+            responseButton.SetActive(true);
+            responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
+            responseButton.GetComponent<Button>().onClick.AddListener(() => OnPickedResponse(response));
+
+            tempResponseContainers.Add(responseContainer);
+        }
+
+        responseBox.gameObject.SetActive(true);
+    }
+
+    private void OnPickedResponse(Response response)
+    {
+        responseBox.gameObject.SetActive(false);
+
+        foreach (GameObject container in tempResponseContainers)
+        {
+            Destroy(container);
+        }
+        tempResponseContainers.Clear();
+
+        if (responseEvents != null)
+        {
+            int responseIndex = System.Array.IndexOf(responseEvents, response);
+            if (responseIndex >= 0 && responseIndex < responseEvents.Length)
+            {
+                responseEvents[responseIndex].OnPickedResponse?.Invoke();
+            }
+        }
+
+        responseEvents = null;
+
+        if (!string.IsNullOrEmpty(response.SceneName))
+        {
+            SceneManager.LoadScene(response.SceneName);
+        }
+        else if (response.DialogueObject)
+        {
+            dialogueUI.ShowDialogue(response.DialogueObject);
+        }
+        else
+        {
+            dialogueUI.CloseDialogueBox();
+        }
+    }
+}
