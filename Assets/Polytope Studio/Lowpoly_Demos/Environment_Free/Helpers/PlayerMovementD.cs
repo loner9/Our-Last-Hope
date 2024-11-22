833bd1f8ea4b182079e52b042b9d669f84aceff8
Loner9 <38490560+loner9@users.noreply.github.com>
2024-11-22 19:50:59 +0700

    Adjustment after merge

--- Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovementD.cs
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovementD.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementD : MonoBehaviour
 {
     public CharacterController controller;
 
