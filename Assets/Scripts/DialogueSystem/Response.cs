e773f082e36199fbd6afb774f671908d010d579b
denx4x <dendymaulanaibrahim@gmail.com>
2024-11-23 11:32:25 +0700

    Fix issue response script

--- Assets/Scripts/DialogueSystem/Response.cs
+++ Assets/Scripts/DialogueSystem/Response.cs
@@ -5,9 +5,9 @@ public class Response
 {
     [SerializeField] private string responseText;
     [SerializeField] private DialogueObject dialogueObject;
-    [SerializeField] private string sceneName; // Tambahkan properti ini
+    [SerializeField] private string sceneName;
 
     public string ResponseText => responseText;
     public DialogueObject DialogueObject => dialogueObject;
-    public string SceneName => sceneName; // Tambahkan getter ini
+    public string SceneName => sceneName;
 }
